54a2204657173943345491a61ceb0b314d04fa13
liadna1995 <liadna1995@gmail.com>
2020-12-27 22:24:11 +0200

    Finish UDP

--- Client.py
+++ Client.py
@@ -5,40 +5,57 @@ import socket
 # def state2_connecting_to_server():
 # def state2_game_mode():
 
+# def Main():
+#     # local host IP '127.0.0.1'
+#     host = '127.0.0.1'
+#
+#     # Define the port on which you want to connect
+#     port = 12345
+#
+#     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#
+#     # connect to server on local computer
+#     s.connect((host, port))
+#
+#     # message you send to server
+#     message = "shaurya says geeksforgeeks"
+#     while True:
+#
+#         # message sent to server
+#         s.send(message.encode('ascii'))
+#
+#         # messaga received from server
+#         data = s.recv(1024)
+#
+#         # print the received message
+#         # here it would be a reverse of sent message
+#         print('Received from the server :', str(data.decode('ascii')))
+#
+#         # ask the client whether he wants to continue
+#         ans = input('\nDo you want to continue(y/n) :')
+#         if ans == 'y':
+#             continue
+#         else:
+#             break
+#     # close the connection
+#     s.close()
+
 def Main():
-    # local host IP '127.0.0.1'
-    host = '127.0.0.1'
+    try:
+        # local host IP '127.0.0.1'
+        ip = '127.0.0.1'
+        port = 13117
+        client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        client.bind(("", 13117))
+        print("Client started, listening for offer requests...")
+        data, addr = client.recvfrom(1024)
+        print(data)
+        print("Received offer from "+ip+","+" attempting to connect...")
+    except:
+        print("bla")
 
-    # Define the port on which you want to connect
-    port = 12345
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # connect to server on local computer
-    s.connect((host, port))
-
-    # message you send to server
-    message = "shaurya says geeksforgeeks"
-    while True:
-
-        # message sent to server
-        s.send(message.encode('ascii'))
-
-        # messaga received from server
-        data = s.recv(1024)
-
-        # print the received message
-        # here it would be a reverse of sent message
-        print('Received from the server :', str(data.decode('ascii')))
-
-        # ask the client whether he wants to continue
-        ans = input('\nDo you want to continue(y/n) :')
-        if ans == 'y':
-            continue
-        else:
-            break
-    # close the connection
-    s.close()
 
 if __name__ == '__main__':
     Main()
--- Server.py
+++ Server.py
@@ -1,13 +1,13 @@
 # import socket programming library
 import socket
-
+import time
 # import thread module
 from _thread import *
 import threading
 
 print_lock = threading.Lock()
 
-#def udp_offer():
+# def udp_offer():
     
 # thread function
 def threaded(c):
@@ -32,33 +32,53 @@ def threaded(c):
     c.close()
 
 
+# def Main():
+#     host = ""
+#
+#     # reverse a port on your computer
+#     # in our case it is 12345 but it
+#     # can be anything
+#     port = 12345
+#     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     s.bind((host, port))
+#     print("socket binded to port", port)
+#
+#     # put the socket into listening mode
+#     s.listen(5)
+#     print("​Server started, listening on IP address")
+#
+#     # a forever loop until client wants to exit
+#     while True:
+#         # establish connection with client
+#         c, addr = s.accept()
+#
+#         # lock acquired by client
+#         print_lock.acquire()
+#         print('Connected to :', addr[0], ':', addr[1])
+#
+#         # Start a new thread and return its identifier
+#         start_new_thread(threaded, (c,))
+#     s.close()
 def Main():
-    host = ""
-
-    # reverse a port on your computer
-    # in our case it is 12345 but it
-    # can be anything
-    port = 12345
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((host, port))
-    print("socket binded to port", port)
-
-    # put the socket into listening mode
-    s.listen(5)
-    print("​Server started, listening on IP address")
-
-    # a forever loop until client wants to exit
-    while True:
-        # establish connection with client
-        c, addr = s.accept()
-
-        # lock acquired by client
-        print_lock.acquire()
-        print('Connected to :', addr[0], ':', addr[1])
-
-        # Start a new thread and return its identifier
-        start_new_thread(threaded, (c,))
-    s.close()
+    try:
+        ip = '127.0.0.1'
+        port = 5005
+        #UDP
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        sock.bind((ip, port))
+        # timeout for 10 seconds
+        sock.settimeout(10)
+        # mode of listening
+        print("​Server started, listening on IP address "+ip)
+        # our message is "offer" to every client who is listening
+        message = b"offer"
+        while True:
+            sock.sendto(message, ('255.255.255.255', 13117))
+            time.sleep(1)
+    except:
+        print("bla")
 
 if __name__ == '__main__':
     Main()
