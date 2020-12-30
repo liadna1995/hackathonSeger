b49d37fc1946afbd6a91a469bc4e9a9a2bb1126d
liadna1995 <66370045+liadna1995@users.noreply.github.com>
2020-12-30 18:45:33 +0200

    Add files via upload
    GO GO GO YAHALOM PAAL

--- Client.py
+++ Client.py
@@ -1,60 +1,101 @@
 # Import socket module
 import socket
 
+# import to game
+# windows
+import msvcrt
+
+
+# linux
+# import sys
+# import select
+# import tty
+# import termios
+
 # def state1_looking_for_server():
 # def state2_connecting_to_server():
 # def state2_game_mode():
+import time
 
-# def Main():
-#     # local host IP '127.0.0.1'
-#     host = '127.0.0.1'
-#
-#     # Define the port on which you want to connect
-#     port = 12345
-#
-#     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#
-#     # connect to server on local computer
-#     s.connect((host, port))
-#
-#     # message you send to server
-#     message = "shaurya says geeksforgeeks"
-#     while True:
-#
-#         # message sent to server
-#         s.send(message.encode('ascii'))
-#
-#         # messaga received from server
-#         data = s.recv(1024)
-#
-#         # print the received message
-#         # here it would be a reverse of sent message
-#         print('Received from the server :', str(data.decode('ascii')))
-#
-#         # ask the client whether he wants to continue
-#         ans = input('\nDo you want to continue(y/n) :')
-#         if ans == 'y':
-#             continue
-#         else:
-#             break
-#     # close the connection
-#     s.close()
 
 def Main():
-    try:
-        # local host IP '127.0.0.1'
-        ip = '127.0.0.1'
-        port = 13117
-        client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        client.bind(("", 13117))
-        print("Client started, listening for offer requests...")
-        data, addr = client.recvfrom(1024)
-        print(data)
-        print("Received offer from "+ip+","+" attempting to connect...")
-    except:
-        print("bla")
+    host = '127.0.0.1'
+    port = 13117
 
+    # the message we suppose to get
+    message = b'\xfe\xed\xbe\xef\x02'
+    while True:
+
+        # UDP socket
+        udpClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        udpClient.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+        try:
+            udpClient.bind(("", port))
+        except socket.error as e:
+            print(str(e))
+
+        # mode of listening
+        print("Client started, listening for offer requests...")
+
+        # receive message from server
+        data, addr = udpClient.recvfrom(1024)
+        # check if this is good message
+        if data[0:4] != message[0:4]:
+            print('Wrong message')
+            continue
+
+        # connect to the server
+        print("Received offer from " + host + ", attempting to connect...")
+
+        # TCP socket
+        tcpClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # connect to the server with the socket that sent in the message
+        tcpClient.connect((host, int(hex(data[5])[2:]+hex(data[6])[2:], 16)))
+        # send the group name
+        tcpClient.sendall(bytes('Bits Magnet\n', 'utf-8'))
+
+        # receive message from server
+        WelcomeData = tcpClient.recvfrom(1024)
+        print(WelcomeData[0].decode("utf-8"))
+
+
+        # windows
+        # timeout after 10 seconds
+        start = time.time()
+        while time.time() - start < 10:
+            if msvcrt.kbhit():
+                keyPress = msvcrt.getch()
+
+                # send the key that pressed
+                tcpClient.sendall(bytes(keyPress, 'utf-8'))
+
+        # linux
+        # def isData():
+        #     return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
+        #
+        # old_settings = termios.tcgetattr(sys.stdin)
+        # try:
+        #     tty.setcbreak(sys.stdin.fileno())
+        #
+        # timeout after 10 seconds
+        # start = time.time()
+        # while time.time() - start < 10:
+        #
+        #         if isData():
+        #             c = sys.stdin.read(1)
+        #             if c == '\x1b':  # x1b is ESC
+        #                 break
+        #
+        # finally:
+        #     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+
+        # receive message from server
+        GameOverData = tcpClient.recvfrom(1024)
+        print(GameOverData[0].decode("utf-8"))
+
+        tcpClient.fileno() == -1
+        print('Server disconnected, listening for offer requests...')
 
 
 if __name__ == '__main__':
--- Server.py
+++ Server.py
@@ -1,84 +1,136 @@
 # import socket programming library
 import socket
 import time
+
 # import thread module
-from _thread import *
-import threading
-
-print_lock = threading.Lock()
-
-# def udp_offer():
-    
-# thread function
-def threaded(c):
-    while True:
-
-        # data received from client
-        data = c.recv(1024)
-        if not data:
-            print('Bye')
-
-            # lock released on exit
-            print_lock.release()
-            break
-
-        # reverse the given string from client
-        data = data[::-1]
-
-        # send back reversed string to client
-        c.send(data)
-
-        # connection closed
-    c.close()
+import _thread
 
 
-# def Main():
-#     host = ""
-#
-#     # reverse a port on your computer
-#     # in our case it is 12345 but it
-#     # can be anything
-#     port = 12345
-#     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     s.bind((host, port))
-#     print("socket binded to port", port)
-#
-#     # put the socket into listening mode
-#     s.listen(5)
-#     print("​Server started, listening on IP address")
-#
-#     # a forever loop until client wants to exit
-#     while True:
-#         # establish connection with client
-#         c, addr = s.accept()
-#
-#         # lock acquired by client
-#         print_lock.acquire()
-#         print('Connected to :', addr[0], ':', addr[1])
-#
-#         # Start a new thread and return its identifier
-#         start_new_thread(threaded, (c,))
-#     s.close()
-def Main():
+def udpBroadcast():
+    host = '127.0.0.1'
+    port = 13117
+
+    # UDP socket
+    udpSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udpSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    udpSock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
     try:
-        ip = '127.0.0.1'
-        port = 5005
-        #UDP
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        sock.bind((ip, port))
-        # timeout for 10 seconds
-        sock.settimeout(10)
-        # mode of listening
-        print("​Server started, listening on IP address "+ip)
-        # our message is "offer" to every client who is listening
-        message = b"offer"
-        while True:
-            sock.sendto(message, ('255.255.255.255', 13117))
-            time.sleep(1)
-    except:
-        print("bla")
+        udpSock.bind((host, port))
+    except socket.error as e:
+        print(str(e))
+
+    # mode of listening
+    print("Server started, listening on IP address " + host)
+
+    # Magic cookie (4 bytes): 0xfeedbeef. The message is rejected if it doesn’t start with this cookie
+    # Message type (1 byte): 0x2 for offer. No other message types are supported
+    # Server port (2 bytes): The port on the server that the client is supposed to connect to over TCP
+
+    # our message to every client who is listening
+    message = b'\xfe\xed\xbe\xef\x02\x13\x8D'
+
+    # timeout after 10 seconds
+    start = time.time()
+    while time.time() - start < 10:
+        # send broadcast one every second
+        udpSock.sendto(message, ('<broadcast>', port))
+        time.sleep(1)
+
+def tcpConnection(tcpSocket, clients):
+    # timeout after 10 seconds
+    start = time.time()
+    while time.time() - start < 10:
+        # get client name
+        clientConn, addr = tcpSocket.accept()
+        data = clientConn.recv(1024)
+        print(data.decode("utf-8"))
+        # save the client in the dict
+        clients[data.decode("utf-8")] = clientConn
+
+def StartGame(clients):
+    # timeout after 10 seconds
+    start = time.time()
+    while time.time() - start < 10:
+        group1 = {}
+        group2 = {}
+        counter = 0
+        welcome = 'Welcome to Keyboard Spamming Battle Royale.\nGroup1:\n==\n'
+
+        for c in clients.keys():
+            if counter % 2 == 0:
+                group1[c] = 0
+            else:
+                group2[c] = 0
+            counter += 1
+
+        for g1 in group1.keys():
+            welcome += g1
+        welcome += 'Group2:\n'
+        # for g2 in group2.keys():
+        #     welcome += g2
+        welcome += '\nStart pressing keys on your keyboard as fast as you can!!\n'
+
+        for clientName, clientConn in clients.items():
+            _thread.start_new_thread(GameMode, (group1, group2, clientConn, welcome,))
+        time.sleep(10.2)
+
+    GameOver = 'Game over!\n'
+    GameOver += 'Group 1 typed in ' + str(sum(group1.values())) + ' characters. Group 2 typed in ' + str(sum(group2.values())) + ' characters.\n'
+    if sum(group1.values()) > sum(group2.values()):
+        GameOver += 'Group 1 wins!\n'
+        GameOver += 'Congratulations to the winners:\n==\n'
+        for g1 in group1.keys():
+            GameOver += g1
+    elif sum(group2.values()) > sum(group1.values()):
+        GameOver += 'Group 2 wins!\n'
+        GameOver += 'Congratulations to the winners:\n==\n'
+        for g2 in group2.keys():
+            GameOver += g2
+    else:
+        GameOver += 'It is a tie!\n'
+
+    for clientConn in clients.values():
+        clientConn.send(bytes(GameOver, 'utf-8'))
+
+def GameMode(clientName, group1, group2, clientConn, welcome):
+    clientConn.send(bytes(welcome, 'utf-8'))
+
+    keyPress = clientConn.recv(1024)
+    if clientName in group1.keys():
+        group1[clientName] += 1
+
+    if clientName in group2.keys():
+        group2[clientName] += 1
+
+
+def Main():
+    clients = {}
+
+    host = '127.0.0.1'
+    port = 2060
+
+    # TCP socket
+    BUFFER_SIZE = 20  # Usually 1024, but we need quick response
+    tcpServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        tcpServer.bind((host, port))
+    except socket.error as e:
+        print(str(e))
+
+    tcpServer.listen(20)
+
+    while True:
+        _thread.start_new_thread(udpBroadcast, ())
+        _thread.start_new_thread(tcpConnection, (tcpServer, clients))
+        time.sleep(10.2)
+        StartGame(clients)
+        tcpServer.close()
+        print('Game over, sending out offer requests...')
+
+
+
 
 if __name__ == '__main__':
     Main()
